1695a3c23563ace09a6a67a6a38442d19e9b4bcf
Linas Mockus <linas.mockus@ongr.io>
2015-02-06 15:16:05 +0200

    Added seo discriminator classes.

---
+++ src/ONGR/DemoOXIDBundle/Entity/SeoArticle.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\DemoOXIDBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ONGR\DemoOXIDBundle\Entity\Seo as Base;
+
+/**
+ * Class SeoArticle
+ * @ORM\Entity(readOnly=true)
+ */
+class SeoArticle extends Base
+{
+    /**
+     * @var \ONGR\DemoOXIDBundle\Entity\Article
+     *
+     * @ORM\ManyToOne(targetEntity="ONGR\DemoOXIDBundle\Entity\Article", inversedBy="seoUrls")
+     * @ORM\JoinColumns({
+     *     @ORM\JoinColumn(name="OXOBJECTID", referencedColumnName="OXID")
+     * })
+     */
+    protected $article;
+
+    /**
+     * Set article
+     *
+     * @param \ONGR\DemoOXIDBundle\Entity\Article $article
+     *
+     * @return Oxseo
+     */
+    public function setArticle(\ONGR\DemoOXIDBundle\Entity\Article $article = null)
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+    /**
+     * Get article
+     *
+     * @return \ONGR\DemoOXIDBundle\Entity\Article
+     */
+    public function getArticle()
+    {
+        return $this->article;
+    }
+}
---
+++ src/ONGR/DemoOXIDBundle/Entity/SeoContent.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\DemoOXIDBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ONGR\DemoOXIDBundle\Entity\Seo as Base;
+
+/**
+ * Class SeoCategory
+ * @ORM\Entity(readOnly=true)
+ */
+class SeoContent extends Base
+{
+    /**
+     * @var \ONGR\DemoOXIDBundle\Entity\Content
+     *
+     * @ORM\ManyToOne(targetEntity="ONGR\DemoOXIDBundle\Entity\Content", inversedBy="seoUrls")
+     * @ORM\JoinColumns({
+     *     @ORM\JoinColumn(name="OXOBJECTID", referencedColumnName="OXID")
+     * })
+     */
+    protected $content;
+
+    /**
+     * Set category
+     *
+     * @param \ONGR\DemoOXIDBundle\Entity\Content $content
+     *
+     * @return Seo
+     */
+    public function setCategory(\ONGR\DemoOXIDBundle\Entity\Content $content = null)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+    /**
+     * Get content.
+     *
+     * @return \ONGR\DemoOXIDBundle\Entity\Content
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
